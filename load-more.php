1a3be2c8144ba6440076b7f715629a20fa15a018
Tomás Colussi <tomascolussi@gmail.com>
2014-04-30 09:50:23 -0600

    Load more issue

--- load-more.php
+++ load-more.php
@@ -2,63 +2,61 @@
 
   $posts = array(
     
-            '<div class="col-md-6 col-sm-6 box padding">'
-            . '<div class="header">'  
-              . '<h2><a href="#">10 most improved teams after NHL trade deadline</a></h2>'    
-              . '<p class="date">March 7, 2014<span>&nbsp;/&nbsp;</span><a href="#">No Comments</a></p>'    
-            . '</div>'  
-            . '<div class="image category4">'  
-              . '<h3 class="category-badge"><a href="#">Sports</a></h3>'    
-              . '<a href="#"><img class="img-responsive" src="images/posts/post5.jpg" alt="Content Post" /></a>'    
-            . '</div>'  
-            . '<p class="excerpt">Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id ultricies ut vehicula ut id. Integer posuere erat a venenatis dapibus posuere velit aliquet duis...</p>'
-          . '</div>',
-
-            '<div class="col-md-6 col-sm-6 box padding">'
-              . '<div class="header">'  
-                . '<h2><a href="#">Obama: Russian intervention completely illegitimate</a></h2>'    
-                . '<p class="date">March 7, 2014<span>&nbsp;/&nbsp;</span><a href="#">No Comments</a></p>'    
-              . '</div>'  
-              . '<div class="image category5">'  
-                . '<h3 class="category-badge"><a href="#">Politics</a></h3>'    
-                . '<a href="#"><img class="img-responsive" src="images/posts/post6.jpg" alt="Content Post" /></a>'  
-              . '</div>'  
-              . '<p class="excerpt">Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id ultricies ut vehicula ut id. Integer posuere erat a venenatis dapibus posuere velit aliquet duis...</p>'
-            . '</div>',
-
-            '<div class="col-md-6 col-sm-6 box padding">'
-            . '<div class="header">'  
-              . '<h2><a href="#">10 most improved teams after NHL trade deadline</a></h2>'    
-              . '<p class="date">March 7, 2014<span>&nbsp;/&nbsp;</span><a href="#">No Comments</a></p>'    
-            . '</div>'  
-            . '<div class="image category4">'  
-              . '<h3 class="category-badge"><a href="#">Sports</a></h3>'    
-              . '<a href="#"><img class="img-responsive" src="images/posts/post5.jpg" alt="Content Post" /></a>'    
-            . '</div>'  
-            . '<p class="excerpt">Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id ultricies ut vehicula ut id. Integer posuere erat a venenatis dapibus posuere velit aliquet duis...</p>'
-          . '</div>',
-
-            '<div class="col-md-6 col-sm-6 box padding">'
-              . '<div class="header">'  
-                . '<h2><a href="#">Obama: Russian intervention completely illegitimate</a></h2>'    
-                . '<p class="date">March 7, 2014<span>&nbsp;/&nbsp;</span><a href="#">No Comments</a></p>'    
-              . '</div>'  
-              . '<div class="image category5">'  
-                . '<h3 class="category-badge"><a href="#">Politics</a></h3>'    
-                . '<a href="#"><img class="img-responsive" src="images/posts/post6.jpg" alt="Content Post" /></a>'  
-              . '</div>'  
-              . '<p class="excerpt">Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id ultricies ut vehicula ut id. Integer posuere erat a venenatis dapibus posuere velit aliquet duis...</p>'
-            . '</div>'
+	'<div class="col-md-6 col-sm-6 box padding">'
+	. '<div class="header">'  
+	  . '<h2><a href="#">10 most improved teams after NHL trade deadline</a></h2>'    
+	  . '<p class="date">March 7, 2014<span>&nbsp;/&nbsp;</span><a href="#">No Comments</a></p>'    
+	. '</div>'  
+	. '<div class="image category4">'  
+	  . '<h3 class="category-badge"><a href="#">Sports</a></h3>'    
+	  . '<a href="#"><img class="img-responsive" src="images/posts/post5.jpg" alt="Content Post" /></a>'    
+	. '</div>'  
+	. '<p class="excerpt">Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id ultricies ut vehicula ut id. Integer posuere erat a venenatis dapibus posuere velit aliquet duis...</p>'
+	. '</div>',
+	
+	'<div class="col-md-6 col-sm-6 box padding">'
+	  . '<div class="header">'  
+		. '<h2><a href="#">Obama: Russian intervention completely illegitimate</a></h2>'    
+		. '<p class="date">March 7, 2014<span>&nbsp;/&nbsp;</span><a href="#">No Comments</a></p>'    
+	  . '</div>'  
+	  . '<div class="image category5">'  
+		. '<h3 class="category-badge"><a href="#">Politics</a></h3>'    
+		. '<a href="#"><img class="img-responsive" src="images/posts/post6.jpg" alt="Content Post" /></a>'  
+	  . '</div>'  
+	  . '<p class="excerpt">Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id ultricies ut vehicula ut id. Integer posuere erat a venenatis dapibus posuere velit aliquet duis...</p>'
+	. '</div>',
+	
+	'<div class="col-md-6 col-sm-6 box padding">'
+	. '<div class="header">'  
+	  . '<h2><a href="#">Walmart enters the auto insurance business</a></h2>'    
+	  . '<p class="date">March 7, 2014<span>&nbsp;/&nbsp;</span><a href="#">No Comments</a></p>'    
+	. '</div>'  
+	. '<div class="image category2">'  
+	  . '<h3 class="category-badge"><a href="#">Money</a></h3>'   
+	  . '<div class="format"><span class="fa fa-picture-o"></span></div>' 
+	  . '<a href="#"><img class="img-responsive" src="images/posts/post7.jpg" alt="Content Post" /></a>'    
+	. '</div>'  
+	. '<p class="excerpt">Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id ultricies ut vehicula ut id. Integer posuere erat a venenatis dapibus posuere velit aliquet duis...</p>'
+	. '</div>',
+	
+	'<div class="col-md-6 col-sm-6 box padding">'
+	  . '<div class="header">'  
+		. '<h2><a href="#">Obama: Russian intervention completely illegitimate</a></h2>'    
+		. '<p class="date">March 7, 2014<span>&nbsp;/&nbsp;</span><a href="#">No Comments</a></p>'    
+	  . '</div>'  
+	  . '<div class="image category5">'  
+		. '<h3 class="category-badge"><a href="#">Politics</a></h3>'    
+		. '<a href="#"><img class="img-responsive" src="images/posts/post8.jpg" alt="Content Post" /></a>'  
+	  . '</div>'  
+	  . '<p class="excerpt">Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id ultricies ut vehicula ut id. Integer posuere erat a venenatis dapibus posuere velit aliquet duis...</p>'
+	. '</div>'
+	
   );
-
 
   $array = array();
   for ($x = 0; $x < count($posts); $x++){
-
     $array[] = $posts[$x];
-
   }
-
 
   //Respuesta en Formato JSON  
   //$returnArray = Array('post' => $post_1);
