c2f8d4549eb198f5804ef7ac86d031fd3e6d3a34
Tomás Colussi <tomascolussi@gmail.com>
2014-05-15 11:42:34 -0600

    Masonry finally fixed for the LOVE OF GOD

--- js/jquery.custom.js
+++ js/jquery.custom.js
@@ -38,7 +38,6 @@ jQuery(document).ready(function($) {
 		$('.masonry').masonry({
 			itemSelector: '.box',
 		  	columnWidth: '.box',
-			transitionDuration  : 0
 		});
 	});
 
@@ -258,59 +257,4 @@ jQuery(document).ready(function($) {
 * author Remy Sharp
 * url http://remysharp.com/2009/01/26/element-in-view-event-plugin/
 */
-(function ($) {
-    function getViewportHeight() {
-        var height = window.innerHeight; // Safari, Opera
-        var mode = document.compatMode;
-
-        if ( (mode || !$.support.boxModel) ) { // IE, Gecko
-            height = (mode == 'CSS1Compat') ?
-            document.documentElement.clientHeight : // Standards
-            document.body.clientHeight; // Quirks
-        }
-
-        return height;
-    }
-
-    $(window).scroll(function () {
-        var vpH = getViewportHeight(),
-            scrolltop = (document.documentElement.scrollTop ?
-                document.documentElement.scrollTop :
-                document.body.scrollTop),
-            elems = [];
-        
-        // naughty, but this is how it knows which elements to check for
-        $.each($.cache, function () {
-            if (this.events && this.events.inview) {
-                elems.push(this.handle.elem);
-            }
-        });
-
-        if (elems.length) {
-            $(elems).each(function () {
-                var $el = $(this),
-                    top = $el.offset().top,
-                    height = $el.height(),
-                    inview = $el.data('inview') || false;
-
-                if (scrolltop > (top + height) || scrolltop + vpH < top) {
-                    if (inview) {
-                        $el.data('inview', false);
-                        $el.trigger('inview', [ false ]);                        
-                    }
-                } else if (scrolltop < (top + height)) {
-                    if (!inview) {
-                        $el.data('inview', true);
-                        $el.trigger('inview', [ true ]);
-                    }
-                }
-            });
-        }
-    });
-    
-    // kick the event to pick up any elements already in view.
-    // note however, this only works if the plugin is included after the elements are bound to 'inview'
-    $(function () {
-        $(window).scroll();
-    });
-})(jQuery);
+(function(b){function t(){var e,a={height:k.innerHeight,width:k.innerWidth};a.height||!(e=l.compatMode)&&b.support.boxModel||(e="CSS1Compat"===e?f:l.body,a={height:e.clientHeight,width:e.clientWidth});return a}function u(){var e=b(),g,q=0;b.each(m,function(a,b){var c=b.data.selector,d=b.$element;e=e.add(c?d.find(c):d)});if(g=e.length)for(d=d||t(),a=a||{top:k.pageYOffset||f.scrollTop||l.body.scrollTop,left:k.pageXOffset||f.scrollLeft||l.body.scrollLeft};q<g;q++)if(b.contains(f,e[q])){var h=b(e[q]),n=h.height(),p=h.width(),c=h.offset(),r=h.data("inview");if(!a||!d)break;c.top+n>a.top&&c.top<a.top+d.height&&c.left+p>a.left&&c.left<a.left+d.width?(p=a.left>c.left?"right":a.left+d.width<c.left+p?"left":"both",n=a.top>c.top?"bottom":a.top+d.height<c.top+n?"top":"both",c=p+"-"+n,r&&r===c||h.data("inview",c).trigger("inview",[!0,p,n])):r&&h.data("inview",!1).trigger("inview",[!1])}}var m={},d,a,l=document,k=window,f=l.documentElement,s=b.expando,g;b.event.special.inview={add:function(a){m[a.guid+"-"+this[s]]={data:a,$element:b(this)};g||b.isEmptyObject(m)||(g=setInterval(u,250))},remove:function(a){try{delete m[a.guid+"-"+this[s]]}catch(d){}b.isEmptyObject(m)&&(clearInterval(g),g=null)}};b(k).bind("scroll resize",function(){d=a=null});!f.addEventListener&&f.attachEvent&&f.attachEvent("onfocusin",function(){a=null})})(jQuery);
