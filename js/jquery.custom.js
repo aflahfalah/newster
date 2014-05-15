e751a1b567384fdb484c8c56aa1999f1a14a40da
Tomás Colussi <tomascolussi@gmail.com>
2014-05-15 11:07:23 -0600

    Update to masonry

--- js/jquery.custom.js
+++ js/jquery.custom.js
@@ -37,7 +37,8 @@ jQuery(document).ready(function($) {
 	$('.masonry').imagesLoaded(function(){
 		$('.masonry').masonry({
 			itemSelector: '.box',
-		  	columnWidth: '.box'		  
+		  	columnWidth: '.box',
+			transitionDuration  : 0
 		});
 	});
 
